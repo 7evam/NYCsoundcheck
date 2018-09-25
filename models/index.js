ebb7e154c5c00a2a4995035d122d09da1b4bbd8a
Evan Lane <evanlane@evam777.home>
2018-09-25 19:10:05 -0400

    worked on database

---
+++ models/index.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize({
+  database: 'nyc_soundcheck',
+  dialect:  'postgres',
+  define:   {
+    underscored:   true,
+    returning:     true,
+  },
+});
+
+const Artist = db.define('artist', {
+  name: {
+    type:      Sequelize.STRING(128),
+    allowNull: false,
+  },
+  desc: {
+  	type: 		Sequelize.TEXT,
+  	allowNull: 	false,
+  },
+  img_url: {
+  	type: 		Sequelize.TEXT,
+  	allowNull: 	false,
+  },
+});
+
+const Genre = db.define('genre', {
+  name: {
+    type:      Sequelize.STRING(64),
+    allowNull: false,
+  },
+});
+
+const Album = db.define('album', {
+  name: {
+    type:      Sequelize.STRING(128),
+    allowNull: false,
+  },
+  img_url: {
+  	type: 		Sequelize.TEXT,
+  	allowNull: 	false,
+  },
+    date: {
+  	type: 		Sequelize.DATEONLY,
+  },
+});
+
+const Upcoming_show = db.define('upcoming_show', {
+  venue: {
+    type:      Sequelize.STRING(64),
+    allowNull: false,
+  },
+    date: {
+  	type: 		Sequelize.DATEONLY,
+  	allowNull:  false,
+  },
+});
+
+//associations
+
+Upcoming_show.belongsToMany(Artist, { through: 'artist_show_xref'});
+Artist.belongsToMany(Upcoming_show, { through: 'artist_show_xref'});
+
+Genre.belongsToMany(Artist, { through: 'artist_genre_xref'});
+Artist.belongsToMany(Genre, { through: 'artist_genre_xref'});
+
+
+Artist.hasMany(Album);
+Album.belongsTo(Artist);
+
+
+module.exports = {
+	Artist, 
+	Genre, 
+	Album, 
+	Upcoming_show, 
+	db,
+};
+
+
